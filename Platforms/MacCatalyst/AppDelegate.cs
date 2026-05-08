f48b02b2b2750560d0caf33a255feef3cf5ddd1a
Jabbah76 <58596571+Jabbah76@users.noreply.github.com>
2026-05-08 14:18:16 +0200

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Repro2987
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
